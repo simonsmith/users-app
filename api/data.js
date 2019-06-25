e70b944ca1b25300083dae311a28e117d2f306af
Simon Smith <simonsmith@users.noreply.github.com>
2019-06-25 19:53:36 +0100

    Add id to fake data that matches one created in json-server

--- api/data.js
+++ api/data.js
@@ -1,9 +1,10 @@
 const faker = require('faker');
 const {sample} = require('lodash');
+const nanoid = require('nanoid');
 
 function createUser() {
   return {
-    id: faker.random.uuid(),
+    id: nanoid(7),
     name: faker.name.findName(),
     username: faker.internet.userName(),
     email: faker.internet.email(),
@@ -15,19 +16,19 @@ function createUser() {
 
 const roles = [
   {
-    id: 'e9b3bc64-1cdf-4722-8eea-decdfc3760de',
+    id: 'wUvgSzC',
     title: 'Senior Developer',
   },
   {
-    id: '63e9f38e-8389-4c56-89bc-f4be72a86c53',
+    id: 'InGBywU',
     title: 'Developer',
   },
   {
-    id: 'a69e4241-ffa7-42ec-a62d-c5d0bf7be054',
+    id: '_EWrS-v',
     title: 'Designer',
   },
   {
-    id: '3f893018-b3b5-408d-9a20-69ee9187e257',
+    id: 'HVfzOi',
     title: 'Product Manager',
   },
 ];
