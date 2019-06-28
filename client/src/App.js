b0fa5d53ed36de4a2634f20adf230fe5d53f36e9
Simon Smith <simonsmith@users.noreply.github.com>
2019-06-28 12:58:21 +0100

    Initial createuser

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {UserList} from './components/user-list';
+import {CreateUser} from './components/create-user';
 
 function App() {
   return (
     <div className="App">
+      <CreateUser />
       <UserList />
     </div>
   );
---
+++ client/src/components/create-user.js
@@ -0,0 +1,7 @@
+import React, {useEffect} from 'react';
+import {useDispatch} from 'react-redux';
+import {createNewUser} from '../store/users';
+
+export function CreateUser() {
+  return <p>create user</p>;
+}
