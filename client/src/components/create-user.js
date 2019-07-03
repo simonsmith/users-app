7a0542b3b1260ad20cb61457d8b1f01fbdfa2f3a
Simon Smith <simonsmith@users.noreply.github.com>
2019-07-03 13:38:13 +0100

    Abstract user creation form into own component

--- client/src/components/create-user.js
+++ client/src/components/create-user.js
@@ -1,7 +1,6 @@
-import React, {useState} from 'react';
-import {useDispatch} from 'react-redux';
+import React from 'react';
 import {createNewUser} from '../store/users';
-import {Input, Select} from './form';
+import {UserEditForm} from './user-edit-form';
 
 export function CreateUser({roleEntities}) {
   const initialInputValues = {
@@ -12,77 +11,16 @@ export function CreateUser({roleEntities}) {
     website: '',
     role: '',
   };
-  const [inputState, setInputState] = useState(initialInputValues);
-  const dispatch = useDispatch();
-
-  const handleInputChange = event => {
-    const {value, id} = event.target;
-    setInputState(state => ({...state, [id]: value}));
+  const onSubmit = (dispatch, user) => {
+    return dispatch(createNewUser(user));
   };
-
-  const handleSubmit = event => {
-    event.preventDefault();
-    const newUser = {
-      ...inputState,
-      role: roleEntities[inputState.role],
-    };
-    dispatch(createNewUser(newUser)).then(() =>
-      setInputState(initialInputValues)
-    );
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <fieldset>
-        <legend>Create a new user</legend>
-        <Input
-          labelText="Name"
-          id="name"
-          value={inputState.name}
-          onChange={handleInputChange}
-        />
-        <Input
-          labelText="Username"
-          id="username"
-          value={inputState.username}
-          onChange={handleInputChange}
-        />
-        <Input
-          labelText="Email"
-          id="email"
-          value={inputState.email}
-          onChange={handleInputChange}
-          type="email"
-        />
-        <Input
-          labelText="Telephone"
-          id="phone"
-          value={inputState.phone}
-          onChange={handleInputChange}
-          type="tel"
-        />
-        <Input
-          labelText="Website"
-          id="website"
-          value={inputState.website}
-          onChange={handleInputChange}
-        />
-        <Select
-          value={inputState.role}
-          onChange={handleInputChange}
-          id="role"
-          labelText="Select a role"
-        >
-          {Object.entries(roleEntities).map(([id, role]) => {
-            return (
-              <option key={id} value={id}>
-                {role.title}
-              </option>
-            );
-          })}
-        </Select>
-        <button>Create</button>
-      </fieldset>
-    </form>
+    <UserEditForm
+      legendText="Create a new user"
+      buttonText="Create"
+      initialInputValues={initialInputValues}
+      onSubmit={onSubmit}
+      roleEntities={roleEntities}
+    />
   );
 }
---
+++ client/src/components/user-edit-form.js
@@ -0,0 +1,77 @@
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import {Input, Select} from './form';
+
+export function UserEditForm({
+  roleEntities,
+  legendText,
+  buttonText,
+  onSubmit,
+  initialInputValues,
+}) {
+  const [inputState, setInputState] = useState(initialInputValues);
+  const dispatch = useDispatch();
+
+  const handleInputChange = event => {
+    const {value, id} = event.target;
+    setInputState(state => ({...state, [id]: value}));
+  };
+
+  const clearForm = () => {
+    const clearedInputs = Object.keys(initialInputValues).reduce((acc, key) => {
+      acc[key] = '';
+      return acc;
+    }, {});
+    setInputState(clearedInputs);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = {
+      ...inputState,
+      role: roleEntities[inputState.role],
+    };
+    onSubmit(dispatch, formData).then(clearForm);
+  };
+
+  const textInputs = [
+    {label: 'Name', id: 'name'},
+    {label: 'Username', id: 'username'},
+    {label: 'Email', id: 'email', type: 'email'},
+    {label: 'Telephone', id: 'phone', type: 'tel'},
+    {label: 'Website', id: 'website'},
+  ];
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <fieldset>
+        <legend>{legendText}</legend>
+        {textInputs.map(input => (
+          <Input
+            key={input.id}
+            labelText={input.label}
+            id={input.id}
+            value={inputState[input.id]}
+            onChange={handleInputChange}
+            type={input.type || 'text'}
+          />
+        ))}
+        <Select
+          value={inputState.role}
+          onChange={handleInputChange}
+          id="role"
+          labelText="Select a role"
+        >
+          {Object.entries(roleEntities).map(([id, role]) => {
+            return (
+              <option key={id} value={id}>
+                {role.title}
+              </option>
+            );
+          })}
+        </Select>
+        <button>{buttonText}</button>
+      </fieldset>
+    </form>
+  );
+}
